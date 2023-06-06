5af94a2bf2dc15eb5c82696334b82faaa76892f1
Marcelo León Cáceres <marcelo.leon.c@gmail.com>
2023-06-06 09:07:10 -0500

    2da Corrección a appsettings.json y Program.cs. Se puso una coma en appsettings.json y se commented out context.Database.Migrate() para las conexiones a Azure

--- ContactWebEFCore6/Program.cs
+++ ContactWebEFCore6/Program.cs
@@ -21,19 +21,19 @@ var azureIdentityDbConnString = builder.Configuration.GetConnectionString("Azuer
 var contextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
     .UseSqlServer(azureIdentityDbConnString)
     .Options;
-using (var context = new ApplicationDbContext(contextOptions))
-{
-    context.Database.Migrate();
-}
+//using (var context = new ApplicationDbContext(contextOptions))
+//{
+//    context.Database.Migrate();
+//}
 
 var azureContactDbConnString = builder.Configuration.GetConnectionString("AzureContactDb");
 var contextOptions2 = new DbContextOptionsBuilder<MyContactManagerDbContext>()
     .UseSqlServer(azureContactDbConnString)
     .Options;
-using (var context = new MyContactManagerDbContext(contextOptions2))
-{
-    context.Database.Migrate();
-}
+//using (var context = new MyContactManagerDbContext(contextOptions2))
+//{
+//    context.Database.Migrate();
+//}
 //
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
